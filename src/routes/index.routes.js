973736ebfa8cd2415de3b911336e5fe6eaaaf22a
Natalia Solorzano <00120319@uca.edu.sv>
2021-11-19 16:56:16 -0600

    Add lista de tareas, handlebars y Partials, Update, Delete, Done

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,23 +1,54 @@
-import {Router} from 'express'
-import Bday from '../models/Bday'
+import { Router } from 'express';
+import Bday from '../models/Bday';
 
-const router = Router()
+const router = Router();
 
-router.get("/", (req, res) => {
-    res.render('index.hbs')
+router.get("/", async (req, res) => {
+    const bdays = await Bday.find().lean();
+
+    res.render('index.hbs', { tasks: bdays });
 });
 router.post('/tasks/add', async (req, res) => {
-    const task = Bday(req.body)
-    await task.save()
-    res.redirect('/')
+    try {
+        const task = Bday(req.body);
+        await task.save();
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get("/about", (req, res) => {
-    res.render('about.hbs')
+    res.render('about.hbs');
 });
 
-router.get("/edit", (req, res) => {
-    res.render('edit.hbs')
+router.get("/edit/:id", async (req, res) => {
+    try {
+        const task = await Bday.findById(req.params.id).lean();
+        res.render('edit.hbs', {task});
+    } catch (error) {
+        console.log(error.message);
+    }
 });
+
+router.post('/edit/:id', async(req, res) => {
+    const {id} = req.params;
+    await Bday.findByIdAndUpdate(id, req.body);
+    res.redirect("/");
+});
+
+router.get('/delete/:id', async(req, res) => {
+    const {id} = req.params;
+    await Bday.findByIdAndDelete(id);
+    res.redirect("/");
+});
+
+router.get('/toggleDone/:id', async(req, res) => {
+    const {id} = req.params;
+    const task = await Bday.findById(id);
+    task.done = !task.done;
+    await task.save();
+    res.redirect('/');
+})
 
 export default router;
